1cd299866d5c16632b9f15321c69c40ae01b3e5a
bwd1992 <brian.douglas09@bathspa.ac.uk>
2015-05-04 17:16:49 +0100

    Added support for images

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,9 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 
 module.exports = function (root) {
-	http.createServer(function (request, response) {
-		router.static(request, response);
-		router.home(request, response);
-	}).listen(port);
-	console.log('Server running at http://127.0.0.1:' + port + '/');
+    http.createServer(function (request, response) {
+        router.static(request, response);
+        router.home(request, response);
+    }).listen(port);
+    console.log('Server running at http://127.0.0.1:' + port + '/');
 };
--- backend/router.js
+++ backend/router.js
@@ -5,48 +5,61 @@ var fs = require('fs');
 //Require the template renderer
 var renderer = require('./render.js');
 
+//Serve static files
+function assets(request, response) {
+    //Give a path for static files
+    var filePath = './' + request.url;
+
+    //We need to change the contentType as we don't want to render the css or js as html
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    //This acts as a mini MIME
+    switch (extname.toLowerCase()) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+        case '.jpg' || '.jpeg':
+            contentType = 'image/jpeg';
+            break;
+    }
+
+    //Handle what to do if path does or does not exist
+    path.exists(filePath, function (exists) {
+        if (exists) {
+            fs.readFile(filePath, function (error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, {'Content-Type': contentType});
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+}
+
 //Make sure the views are rendered as html
 var header = {
     'Content-Type': 'text/html'
 };
-
-//Serve static files
-function static(request, response) {
-	//Give a path for static files
-	var filePath = './' + request.url;
-
-	//We need to change the contentType as we don't want to render the css or js as html
-	var extname = path.extname(filePath);
-	var contentType = 'text/html';
-	switch (extname) {
-		case '.js':
-			contentType = 'text/javascript';
-			break;
-		case '.css':
-			contentType = 'text/css';
-			break;
-	}
-
-	//Handle what to do if path does or does not exist
-	path.exists(filePath, function (exists) {
-		if (exists) {
-			fs.readFile(filePath, function (error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				}
-				else {
-					response.writeHead(200, { 'Content-Type': contentType });
-					response.end(content, 'utf-8');
-				}
-			});
-		}
-		else {
-			response.writeHead(404);
-			response.end();
-		}
-	});
-}
 
 //Handle HTTP route GET '/' and POST '/'
 function home(request, response) {
@@ -71,4 +84,4 @@ function home(request, response) {
 
 //Export our routes
 module.exports.home = home;
-module.exports.static = static;
+module.exports.static = assets;
